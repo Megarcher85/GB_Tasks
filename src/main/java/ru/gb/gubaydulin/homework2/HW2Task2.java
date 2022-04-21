bcf2e39607ce949dfc018c7c0ee4f430610ce7b5
Megaarcher85 <megaarcher85@gmail.com>
2022-04-21 17:34:41 +0300

    HW2 Task1

---
+++ src/main/java/ru/gb/gubaydulin/homework2/HW2Task2.java
@@ -0,0 +1,21 @@
+package ru.gb.gubaydulin.homework2;
+
+import java.util.Scanner;
+
+public class HW2Task2 {
+    public static void main(String[] args) {
+        System.out.println ("Введите любое число");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        checkPozitive(a);
+    }
+
+    public static void checkPozitive(int a) {
+
+        if ( a >=0 ){
+            System.out.println("Число положительное");
+        } else{
+            System.out.println("Число отрицательное");
+        }
+    }
+}
