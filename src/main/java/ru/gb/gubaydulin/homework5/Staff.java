a5e7fcf6f466db9dd07807c7059ce5ac3c860dc7
Megaarcher85 <megaarcher85@gmail.com>
2022-05-14 23:32:51 +0300

    HW5 Task2 (попробовал создать объект в исходном и в другом классе)

---
+++ src/main/java/ru/gb/gubaydulin/homework5/Staff.java
@@ -0,0 +1,9 @@
+package ru.gb.gubaydulin.homework5;
+
+public class Staff {
+    public static void main(String[] args) {
+        Task1.Employee employee = new Task1.Employee("Иванов Иван Иванович", "менеджер", "iii@mail.ru", "1111", 50000, 25);
+
+
+    }
+}
--- src/main/java/ru/gb/gubaydulin/homework5/Task1.java
+++ src/main/java/ru/gb/gubaydulin/homework5/Task1.java
@@ -4,13 +4,25 @@ public class Task1 {
     public static void main(String[] args) {
 
     }
-        public class Employee {
-            private String fullName;
-            private String position;
-            private String email;
-            private String phoneNumb;
-            private int salary;
-            private int age;
+
+    public static class Employee {
+        private String fullName;
+        private String position;
+        private String email;
+        private String phoneNumb;
+        private int salary;
+        private int age;
+
+        public Employee (String fullName, String position, String email, String phoneNumb, int salary, int age) {
+            this.fullName = fullName;
+            this.position = position;
+            this.email = email;
+            this.phoneNumb = phoneNumb;
+            this.salary = salary;
+            this.age = age;
         }
+
     }
+    Employee employee = new Employee ("Петров Пётр Петрович", "начальник отдела", "ppp@mail.ru", "2222", 60, 35);
+}
 
