9f7076a97e6c3cd525bc8a70849932b8eeaceedd
Megaarcher85 <megaarcher85@gmail.com>
2022-05-16 00:38:29 +0300

    HW5 Task2 (вывел в консоль для проверки)

--- src/main/java/ru/gb/gubaydulin/homework5/Task1.java
+++ src/main/java/ru/gb/gubaydulin/homework5/Task1.java
@@ -1,28 +1,35 @@
 package ru.gb.gubaydulin.homework5;
 
+import java.lang.*;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        Employee employee1 = new Employee("Иванов Иван Иванович", "менеджер", "iii@mail.ru", "1111", 50000, 25);
+        Employee employee2 = new Employee("Петров Пётр Петрович", "начальник отдела", "ppp@mail.ru", "2222", 60, 35);
+
+
+        System.out.println("employee1_FullName = " + employee1.getFullName());
+        System.out.println("employee1_position = " + employee1.getPosition());
+        System.out.println("employee1_email = " + employee1.getEmail());
+        System.out.println("employee1_phoneNumb = " + employee1.getPhoneNumb());
+        System.out.println("employee1_salary = " + employee1.getSalary());
+        System.out.println("employee1_age = " + employee1.getAge());
+        System.out.println();
+
+        System.out.println("employee2_FullName = " + employee2.getFullName());
+        System.out.println("employee2_position = " + employee2.getPosition());
+        System.out.println("employee2_email = " + employee2.getEmail());
+        System.out.println("employee2_phoneNumb = " + employee2.getPhoneNumb());
+        System.out.println("employee2_salary = " + employee2.getSalary());
+        System.out.println("employee2_age = " + employee2.getAge());
+
     }
-
-    public static class Employee {
-        private String fullName;
-        private String position;
-        private String email;
-        private String phoneNumb;
-        private int salary;
-        private int age;
-
-        public Employee (String fullName, String position, String email, String phoneNumb, int salary, int age) {
-            this.fullName = fullName;
-            this.position = position;
-            this.email = email;
-            this.phoneNumb = phoneNumb;
-            this.salary = salary;
-            this.age = age;
-        }
-
-    }
-    Employee employee = new Employee ("Петров Пётр Петрович", "начальник отдела", "ppp@mail.ru", "2222", 60, 35);
 }
+
+
+
+
+
+
 
