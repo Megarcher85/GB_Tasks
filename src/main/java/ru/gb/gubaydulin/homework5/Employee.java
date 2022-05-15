f228e7d7cfadeef463993a2d328a0eec424cea64
Megaarcher85 <megaarcher85@gmail.com>
2022-05-15 14:40:03 +0300

    HW5 Task1

---
+++ src/main/java/ru/gb/gubaydulin/homework5/Employee.java
@@ -0,0 +1,13 @@
+package ru.gb.gubaydulin.homework5;
+
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumb;
+    private int salary;
+    private int age;
+}
+
+
