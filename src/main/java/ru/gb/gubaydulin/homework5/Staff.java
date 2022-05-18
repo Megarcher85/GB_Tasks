efa158bf98ac458bfd69b56c5366f14490976ac1
Megarcher85 <103512044+Megarcher85@users.noreply.github.com>
2022-05-18 16:07:23 +0300

    Delete Staff.java

--- src/main/java/ru/gb/gubaydulin/homework5/Staff.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.gubaydulin.homework5;
-
-public class Staff {
-    public static void main(String[] args) {
-        Task1.Employee employee = new Task1.Employee("Иванов Иван Иванович", "менеджер", "iii@mail.ru", "1111", 50000, 25);
-
-
-    }
-}
