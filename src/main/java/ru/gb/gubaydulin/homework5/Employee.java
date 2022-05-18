5e3d649e64de7ead2233b6d0dd82c839e9cf81c3
Megaarcher85 <megaarcher85@gmail.com>
2022-05-18 17:15:23 +0300

    HW5 Task2 (конструктор Employee)

--- src/main/java/ru/gb/gubaydulin/homework5/Employee.java
+++ src/main/java/ru/gb/gubaydulin/homework5/Employee.java
@@ -8,4 +8,13 @@ public class Employee {
     private String phoneNumb;
     private int salary;
     private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumb, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumb = phoneNumb;
+        this.salary = salary;
+        this.age = age;
+    }
 }
