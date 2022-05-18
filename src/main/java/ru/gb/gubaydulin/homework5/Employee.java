f1f3d523336b2a491de0c59bd1a0e34b053f4ceb
Megaarcher85 <megaarcher85@gmail.com>
2022-05-18 17:12:15 +0300

    HW5 Task1 (класс Employee)

--- src/main/java/ru/gb/gubaydulin/homework5/Employee.java
+++ src/main/java/ru/gb/gubaydulin/homework5/Employee.java
@@ -8,34 +8,4 @@ public class Employee {
     private String phoneNumb;
     private int salary;
     private int age;
-
-
-    public Employee(String fullName, String position, String email, String phoneNumb, int salary, int age) {
-        this.fullName = fullName;
-        this.position = position;
-        this.email = email;
-        this.phoneNumb = phoneNumb;
-        setSalary(salary);
-        setAge(age);
-    }
-
-    public void setSalary(int salary) {
-        if (salary >= 0) {
-            this.salary = salary;
-        }
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age >= 0 && age < 200) {
-            this.age = age;
-        }
-    }
-
-    public void print() {
-        System.out.println("Полное имя сотрудника: " + fullName + ", " + "Должность сотрудника: " + position + ", " + "Электронная почта: " + email + ", " + "Номер телефона: " + phoneNumb + ", " + "Заработная плата: " + salary + ", " + "Возраст сотрудника: " + age);
-    }
 }
