54dcd794c244c4784464b40d1c718ad922c9415b
Megaarcher85 <megaarcher85@gmail.com>
2022-05-18 12:21:40 +0300

    HW5 Task-3 (вывод в консоль)

--- src/main/java/ru/gb/gubaydulin/homework5/Employee.java
+++ src/main/java/ru/gb/gubaydulin/homework5/Employee.java
@@ -9,4 +9,29 @@ public class Employee {
     private int salary;
     private int age;
 
+
+    public Employee(String fullName, String position, String email, String phoneNumb, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumb = phoneNumb;
+        setSalary(salary);
+        setAge(age);
     }
+
+    public void setSalary(int salary) {
+        if (salary >= 0) {
+            this.salary = salary;
+        }
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age < 200) {
+            this.age = age;
+        }
+    }
+
+    public void print() {
+        System.out.println("Полное имя сотрудника: " + fullName + ", " + "Должность сотрудника: " + position + ", " + "Электронная почта: " + email + ", " + "Номер телефона: " + phoneNumb + ", " + "Заработная плата: " + salary + ", " + "Возраст сотрудника: " + age);
+    }
+}
